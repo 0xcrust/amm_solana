88392042726d794a5b20a1cb045d2581ef1fcc9d
crustydev <ademola1145@gmail.com>
2022-10-13 22:06:36 +0100

    Write helper code for client testing

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,7 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 import * as spl from '@solana/spl-token';
 
-export const createTokenMint = async (connection: anchor.web3.Connection, mintAuthority: anchor.web3.Keypair)
+export const createTokenMint = async (connection: anchor.web3.Connection, mintAuthority: anchor.web3.Keypair, decimals: number)
 : Promise<[anchor.web3.PublicKey, anchor.web3.Keypair]>  => {
 
     const airdropSignature3 = await connection.requestAirdrop(
@@ -21,7 +21,7 @@ export const createTokenMint = async (connection: anchor.web3.Connection, mintAu
       mintAuthority,
       mintAuthority.publicKey,
       null,
-      0
+      decimals
     );
     console.log(`Mint account created with address: ${mintAddress.toBase58()}`);
   
@@ -29,9 +29,8 @@ export const createTokenMint = async (connection: anchor.web3.Connection, mintAu
 }
 
 
-export const airdrop = async (connection, destinationWallet: anchor.web3.Keypair, amount) => {
-    const airdropSignature = await connection.requestAirdrop(destinationWallet
-        .publicKey, amount * anchor.web3.LAMPORTS_PER_SOL);
+export const airdrop = async (connection: anchor.web3.Connection, destinationWallet: anchor.web3.PublicKey, amount) => {
+    const airdropSignature = await connection.requestAirdrop(destinationWallet, amount * anchor.web3.LAMPORTS_PER_SOL);
 
     const latestBlockHash = await connection.getLatestBlockhash();
 
@@ -40,5 +39,33 @@ export const airdrop = async (connection, destinationWallet: anchor.web3.Keypair
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
       signature: airdropSignature,
     });
-    console.log(`Airdropped ${amount} sol to ${destinationWallet.publicKey}!`);
+    console.log(`Airdropped ${amount} sol to ${destinationWallet}!`);
+}
+
+export const createATA = async(connection: anchor.web3.Connection, account: anchor.web3.Keypair, mint: anchor.web3.PublicKey)
+: Promise<anchor.web3.PublicKey> => {
+    const wallet = await spl.createAssociatedTokenAccount(
+        connection,
+        account,
+        mint,
+        account.publicKey
+    );
+
+    console.log("Created Associated Token Account");
+    return wallet;
+}
+
+export const mintTokensToWallet = async(connection: anchor.web3.Connection, wallet: anchor.web3.PublicKey, 
+    amount: number, feePayer: anchor.web3.Keypair, mintAddress: anchor.web3.PublicKey, mintAuthority: anchor.web3.Keypair) => {
+    let tx = await spl.mintToChecked(
+        connection,
+        feePayer,
+        mintAddress,
+        wallet,
+        mintAuthority,
+        amount * 1e0,
+        0
+    );
+
+    console.log(`Minted ${amount} tokens to ${wallet}`);
 }
